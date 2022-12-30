ab352f0c5a602fb2632572904387323c0a3777b4
Roy Vanegas <roy@thecodeeducators.com>
2022-12-30 13:03:14 -0500

    Update app.js

--- web-linter-test-project/js/app.js
+++ web-linter-test-project/js/app.js
@@ -1,3 +1,15 @@
 window.onload = () => {
   alert('Hello World!');
+
+  switch (foo) {
+    case 1:
+
+            break;
+        case 2:
+
+            break;
+        default:
+
+            break;
+    }
 }
